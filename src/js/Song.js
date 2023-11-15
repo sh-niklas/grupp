0094116b210860418bc85a5569876cf4cdb7f79e
Cralcas <carlosbergq@gmail.com>
2023-11-15 14:40:08 +0100

    Added song cards

---
+++ src/js/Song.js
@@ -0,0 +1,11 @@
+export class Song {
+  name;
+  artist;
+  country;
+
+  constructor(name, artist, country) {
+    this.name = name;
+    this.artist = artist;
+    this.country = country;
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,35 @@
-import './../scss/style.scss'
+import "./../scss/style.scss";
+import {Song} from "./Song";
+
+const song1 = new Song("En kväll i juni", "Lasse Berghagen", "Sweden");
+const song2 = new Song("Firework", "Katy Perry", "USA");
+const song3 = new Song("Nessun dorma", "Luciano Pavarotti", "Italy");
+
+let songs = [song1, song2, song3];
+
+const createSongHtml = () => {
+  const songContainer = document.getElementById("songContainer");
+
+  songs.forEach((song) => {
+    const songCard = document.createElement("div");
+    const songTitle = document.createElement("h3");
+    const artistTitle = document.createElement("h4");
+    const countryTitle = document.createElement("p");
+
+    songCard.id = "songCard";
+    songTitle.className = "songTitle";
+    artistTitle.className = "artistTitle";
+    countryTitle.className = "contryTitle";
+
+    songTitle.innerHTML = song.name;
+    artistTitle.innerHTML = song.artist;
+    countryTitle.innerHTML = song.country;
+
+    songCard.appendChild(songTitle);
+    songCard.appendChild(artistTitle);
+    songCard.appendChild(countryTitle);
+    songContainer.appendChild(songCard);
+    document.body.appendChild(songContainer);
+  });
+};
+createSongHtml();
